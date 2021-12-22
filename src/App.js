223521a161b847852685f79e3643318061cb6ea7
shreyalkhaniya97 <shreyalkhaniya97@gmail.com>
2021-12-22 14:16:18 +0530

    update our menu-items to navigate withrouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from "react";
+import {
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+  useMatch,
+} from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
+
+const HatsPage = (props) => {
+  return (
+    <div>
+      <Link to='/topics/12'>Topics</Link>
+      <h1>HATS PAGE {props.id}</h1>
+    </div>
+  );
+};
+
+const TopicDetail = (props) => {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+  let match = useMatch(location.pathname);
+  console.log({ navigate });
+  console.log({ location });
+  console.log(`match`, match);
+  return (
+    <div>
+      <button onClick={() => navigate("/hats")}>Hats</button>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+        <Route path='/topics/:id' element={<TopicDetail />} />
+      </Routes>
     </div>
   );
 }
